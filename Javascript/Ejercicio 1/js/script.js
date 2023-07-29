1c8d755da0cef9320ce9e42f346701bb07700815
YeisonTorres-G <yeisontorres1205@gmail.com>
2023-07-29 17:11:32 -0500

    Se Terminaron ejercicios de JavaScript

---
+++ Javascript/Ejercicio 1/js/script.js
@@ -0,0 +1,22 @@
+console.log("Analisis Y Desarrollo de Software");
+
+
+/* Unir dos cadenas de texto
+const nom="Yeison"
+const ape="Torres"
+alert("Mi nombre es "+nom+" y mi apellido es "+ape) */
+/*
+const num1=5
+const num2=2
+const suma = num1+num2
+parseInt(suma)
+//alert("la suma es: "+suma)
+const div = num1/num2
+alert("la multi es "+div)*/
+
+let nom=prompt("Escriba el Nombre y apellidos")
+let nom2=prompt("Escriba su edad")
+let nom3=prompt("Escriba su Peso")
+let nom4=prompt("Escriba la Direccion")
+
+alert("Mi nombre es "+nom+" tengo "+nom2+" años, mi peso es "+nom3+" kilos y vivo en la direccion "+nom4)
---
+++ Javascript/Ejercicio 2/js/script2.js
@@ -0,0 +1,189 @@
+/*Dado el sueldo de un empleado imprimir en una alerta el nuevo sueldo del empleado si tuvo un aumento de 10% 
+
+const  sueldo = prompt("Ingrese el valor del Sueldo ");
+const psueldo=10/100
+const num=sueldo
+alert("su sueldo es "+sueldo1);  
+
+
+un vendendedor tiene su sueldo base, recibe una comicion de 30% total de las ventas del mes quiere saber cuanto ganara en un mes que tubo 3 ventas
+
+  const Sueldo=prompt("Ingrese Sueldo")
+  const venta1=prompt("Ingrese venta 1")
+  const venta2=prompt("Ingrese venta 2")
+  const venta3=prompt("Ingrese venta 3")
+
+  const vsuma=parseInt(venta1)+parseInt(venta2)+parseInt(venta3)
+  
+  alert("Sus ganancias de este mes son: "+vsuma)
+
+
+
+
+  1. Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra.
+
+const compra=prompt("Ingrese total a pagar")
+const pcompra=25/100;
+const tcomp= parseInt(compra)-parseInt(compra*pcompra);
+alert("El total a pagar es: "+tcomp)
+
+
+
+
+2. Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual.
+
+const inv=prompt("Ingrese Valor a Invertir")
+const pinv=2/100;
+const multi=parseInt(inv*pinv);
+const tinv=parseInt(inv)+parseInt(multi)
+alert("las ganancias de la invercion son: "+multi)
+
+
+
+
+3 Un alumno desea saber cuál será su calificación final en la materia de
+Algoritmos. Dicha calificación se compone de los siguientes porcentajes:
+a. 55% del promedio de sus tres calificaciones parciales.
+b. 30% de la calificación del examen final.
+c. 15% de la calificación de un trabajo final.
+
+const nota1 =prompt("Ingrese la nota 1");
+const nota2 =prompt("Ingrese la nota 2");
+const nota3 =prompt("Ingrese la nota 3");
+
+const prom=parseInt(nota1)+parseInt(nota2)+parseInt(nota3)
+const div = parseInt(prom)/3
+alert("Su nota es "+div)
+
+const pnot1= 55 / 100;
+const ppromenot1 = div * pnot1
+alert("El 55% de sus notas es :"+ppromenot1);
+
+const nota4 =prompt("Ingrese la nota 4");
+const pnot2= 30 / 100;
+const ppromenot2 = nota4 * pnot2;
+alert("El 30% de sus notas es :"+ppromenot2);
+
+const nota5 =prompt("Ingrese la nota 5");
+const pnot3= 15 / 100;
+const ppromenot3 = nota5 * pnot3;
+alert("El 30% de sus notas es :"+ppromenot3);
+
+const notaf= parseInt(ppromenot1)+parseInt(ppromenot2)+parseInt(ppromenot3);
+const notaf1= notaf / 3;
+
+alert("La nota final de algoritmo es: "+notaf1)
+
+
+
+
+
+4. El dueño de una tienda compra un artículo a un precio determinado. Obtener el
+precio en que lo debe vender para obtener una ganancia del 30%.
+
+const preciop=prompt("ingrese Precio del producto")
+const pprecio= 30/100
+const ppreciof= preciop * pprecio
+const preciof = parseInt(preciop)+parseInt(ppreciof)
+alert("El precio que debe vender el producto es: "+preciof)
+
+
+
+
+
+5.Un taxi cobra por una carrera 15 mil pesos por kilómetro recorrido y 2 mil por
+minuto. Determine el monto a pagar por una carrera.
+
+const kil=prompt("Ingrese Kilometros recorridos")
+const mink=prompt("Ingrese miutos")
+const kil1 = 15000
+const min1 = 2000
+
+const kilo= kil * kil1
+const mino= mink * min1
+
+const pagar=parseInt(kilo)+parseInt(mino);
+
+alert("El monto a pagar es: "+pagar)
+
+
+
+
+6. Realizara un programa en el cual se van a leer tres números y determinar el
+cuadrado de cada uno de ellos.
+
+const num1 = prompt("Ingrese numero 1 ")
+const cuadrado = num1*num1
+alert("El cuadrado es: "+cuadrado)
+
+const num2 = prompt("Ingrese numero 2 ")
+const cuadrado2 = num2*num2
+alert("El cuadrado es: "+cuadrado2)
+
+const num3 = prompt("Ingrese numero 3 ")
+const cuadrado3 = num3*num3
+alert("El cuadrado es: "+cuadrado3)
+
+
+
+
+7.En un hospital existen tres áreas: Ginecología, Pediatría, Traumatología. El
+presupuesto anual del hospital se reparte conforme a la sig. tabla:
+Área Porcentaje del presupuesto
+Ginecología 40%
+Traumatología 30%
+Pediatría 30%
+
+const presu=prompt("Ingrese Presupuesto")
+const gin= 40/100
+const gin1= presu*gin
+alert("El presupuesto para ginecologia es:"+gin1)
+
+
+const trau= 30/100
+const trau2= presu*trau
+alert("El presupuesto para traumatologia es:"+trau2)
+
+const ped= 30/100
+const ped1= presu*ped
+alert("El presupuesto para pediatria es:"+ped1)
+
+
+
+
+8.Tres personas deciden invertir su dinero para fundar una empresa. Cada una de
+ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte
+con respecto a la cantidad total invertida.
+
+const inv1=prompt("ingresar invercio 1")
+const inv2=prompt("ingresar invercio 2")
+const inv3=prompt("ingresar invercio 3")
+const sumainv=parseInt(inv1)+parseInt(inv2)+parseInt(inv3);
+
+const multi= inv1 * 100
+const div= multi / sumainv
+alert("el porcentage de la persona uno es: "+div+"%")
+
+const multi1= inv2 * 100
+const div1= multi1 / sumainv
+alert("el porcentage de la persona dos es: "+div1+"%")
+
+const multi2= inv3 * 100
+const div2= multi2 / sumainv
+alert("el porcentage de la persona tres es: "+div2+"%")*/
+
+
+
+
+
+
+
+
+
+
+
+
+
